f83605e76d4cf1a6bee0c76f57838a12f6028e70
Austin Downing <HanSolo552@gmail.com>
2018-10-29 09:50:40 -0400

    Add files via upload

---
+++ Kill Process.ps1
@@ -0,0 +1,12 @@
+﻿#Gets all workstations that need to have software installed, if you don't want to uninstall all of the software from you will need to use a text document and Get-Content
+$Machines = Get-ADComputer -Filter * -SearchBase "OU=Workstations,DC=Contoso,DC=COM" | Select DNSHostName -ExpandProperty DNSHostname
+
+#Name of Application To Kill
+$Application = "Notepad"
+
+#For Each Loop to Kill Process On Each Computer
+
+foreach ($Machine in $Machines) {
+
+(Get-WmiObject Win32_Process -ComputerName $Machine | ?{ $_.ProcessName -match "$Application" }).Terminate()
+}
